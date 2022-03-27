5ee93316274c6715af669682c5b0e83c693903c8
xiaoxiao <3147264966@qq.com>
2022-03-27 22:21:00 +0800

    全局组件 <Nav/>

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,10 +1,12 @@
 <template>
-    <div>Statistics.vue</div>
+    <div>Statistics.vue
+        <Nav/>
+    </div>
 </template>
 
-<script>
+<script lang="ts">
     export default {
-        name: 'Statistics'
+        name: 'Statistics',
     }
 </script>
 
