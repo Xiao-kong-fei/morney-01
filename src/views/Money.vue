ef08c684c5b6edbd00e6a44b96553f8ae7661edf
xiaoxiao <3147264966@qq.com>
2022-04-24 00:02:50 +0800

    将所有数据封装在 store 里

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,19 +18,15 @@
     import Types from '@/components/Money/Types.vue';
     import FormItem from '@/components/Money/FormItem.vue';
     import Tags from '@/components/Money/Tags.vue';
-    import {Component, Watch} from 'vue-property-decorator';
-    import recordListModel from '@/models/recordListModel';
-    import tagListModel from '@/models/tagListModel';
-
-    const recordList = recordListModel.fetch();
-    const tagList = tagListModel.fetch();
+    import {Component} from 'vue-property-decorator';
+    import store from '../store/index2';
 
     @Component({
     components: {Tags, FormItem, Types, NumberPad}
     })
     export default class Money extends Vue{
-        tags = tagList;
-        recordList: RecordItem[] = recordList;
+        tags = store.tagList;
+        recordList = store.recordList;
         record: RecordItem = {
             tags: [], notes: '', type: '-', amount: 0
         };
@@ -42,11 +38,7 @@
             this.record.notes = value;
         }
         saveRecord() {
-            recordListModel.create(this.record);
-        }
-        @Watch('recordList')
-        onRecordListChange() {
-            recordListModel.save();
+            store.createRecord(this.record);
         }
     }
 </script>
