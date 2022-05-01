a8831c75791c740836edc231cdcd4d490eb10575
xiaoxiao <3147264966@qq.com>
2022-05-01 21:17:36 +0800

    Money.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,7 +4,7 @@
             <button @click="create">新增标签</button>
         </div>
         <ul class="current">
-            <li v-for="tag in dataSource" :key="tag.id"
+            <li v-for="tag in tagList" :key="tag.id"
                 :class="{selected: selectedTags.indexOf(tag)>=0}"
                 @click="toggle(tag)">{{tag.name}}
             </li>
@@ -16,9 +16,15 @@
 import Vue from "vue";
 import {Component, Prop} from "vue-property-decorator";
 
-@Component
+@Component({
+    computed: {
+        tagList(){
+            // return this.$store.fetchTags();
+            return[];
+        }
+    }
+})
 export default class Tags extends Vue {
-    @Prop() readonly dataSource: string[] | undefined;
     selectedTags: string[] = [];
     toggle(tag: string){
         const index = this.selectedTags.indexOf(tag);
@@ -31,12 +37,8 @@ export default class Tags extends Vue {
     }
     create() {
         const name = window.prompt('请输入一个标签名');
-        if(name === '') {
-            window.alert('标签名不能为空')
-        }else if (this.dataSource) {
-            this.$emit('update:dataSource', 
-            [...this.dataSource, name]);
-        }
+        if(!name) { return window.alert('标签名不能为空'); }
+        // store.createTag(name);
     }
 }
 </script>
