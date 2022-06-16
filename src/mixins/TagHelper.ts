0e95a2057149876655a3a450d89d8b17845aa814
xiaoxiao <3147264966@qq.com>
2022-06-17 01:56:03 +0800

    修复进页面就提示的 bug

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Component from 'vue-class-component';
 
+const map: {[key: string]: string} = {
+    'tag name duplicated': '标签名重复了'
+};
+
 @Component
 export class TagHelper extends Vue {
     hello = 'Hello'
@@ -8,6 +12,11 @@ export class TagHelper extends Vue {
         const name = window.prompt('请输入一个标签名');
         if(!name) { return window.alert('标签名不能为空'); }
         this.$store.commit('createTag', name);
+        if(this.$store.state.createTagError) {
+            if(this.$store.state.createTagError) {
+                window.alert(map[this.$store.state.createTagError.message] || '未知错误');
+            }
+        }
     }
 }
 export default TagHelper;
